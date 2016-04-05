340036efbfd5f8a4d7d3fc10ec3654f4bb770b10
unknown <tony.lei@WRK-IT-40.zepower.com>
2016-04-05 14:57:13 -0700

    update connection

--- Client/connection.js
+++ Client/connection.js
@@ -1,20 +1,16 @@
-	var net = require('net');
-	var buffer = require('buffer');
-
+var net = require('net');
+var buffer = require('buffer');
+var buff;
+var respBuff;
 
   function connect () {
-    var client = new net.Socket();
-	var buff = new buffer.Buffer(64);
-	client.messageSize = 0;
-	client.messageReadSize = 0;
-	//buff.allocUnsafe(4);
-	var respBuff = new buffer.Buffer(0, 'hex');
+        var client = new net.Socket();
+	    buff = new buffer.Buffer(64);
+	    client.messageSize = 0;
+	    client.messageReadSize = 0;
+	    respBuff = new buffer.Buffer(0);
 	
-
-	
-	
-	
-	client.connect(10001, '10.0.1.23', function() {
+	    client.connect(10001, '10.0.1.23', function() {
 		console.log('Connected');
 		buff.writeUInt8(0x1,7);
 		buff.write('tony', 8 );
@@ -23,20 +19,18 @@
 	});
 
 	client.on('data', function(data) {
-		console.log('Received: ' + data);
-		if(client.bufferSize == 0){
-			
-			client.messageSize = data.readInt32LE(0);
-		}
+	    console.log('Received: ' + data);
+	    if(client.bufferSize == 0){
+		    client.messageSize = data.readInt32LE(0);
+	    }
+	    client.messageReadSize += data.length;
+	    respBuff = Buffer.concat([respBuff, data], respBuff.length + data.length);
 		
-		client.messageReadSize += data.length;
-		buffer.Buffer.concat([respBuff, data]);
+	    if(client.messageReadSize >= client.messageSize + 64){
+		    displayProducts(respBuff);
+	    }
 		
-		if(client.messageReadSize >= client.messageSize + 64){
-			displayProducts(respBuff);
-		}
-		
-		//client.destroy(); // kill client after server's response
+	    //client.destroy(); // kill client after server's response
 		
 		
 	});
@@ -56,5 +50,5 @@
 function displayProducts(data){
 	
 	var div = document.getElementById('products');
-	div.innerHTML = data.slice(63);
+	div.innerHTML = data.slice(64);
 }

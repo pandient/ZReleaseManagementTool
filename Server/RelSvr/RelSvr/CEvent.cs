06f623762c3a9ca0adf4887111f5b541c04c90c7
hajime1 <hajime.kobayashi@ze.com>
2016-04-06 14:48:58 -0700

    server

---
+++ Server/RelSvr/RelSvr/CEvent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace RelSvr
+{
+    internal class CEvent
+    {
+        public static object m_lock = new object();
+        public static string m_message = string.Empty;
+
+        private static ManualResetEvent m_receiveDone = new ManualResetEvent(false);
+
+        private CEvent()
+        {
+        }
+
+        public static void Wait()
+        {
+            m_receiveDone.WaitOne();
+        }
+
+        public static void Set()
+        {
+            m_receiveDone.Set();
+        }
+
+        public static void Reset()
+        {
+            m_receiveDone.Reset();
+        }
+    }
+}
